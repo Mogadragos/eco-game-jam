d48851055df414f38c4cf67ed7be2fcf1661e72c
Quentin <quentin.robard@beecoming.fr>
2022-01-08 14:32:27 +0100

    has turtle

--- src/model/Enemy.mjs
+++ src/model/Enemy.mjs
@@ -4,12 +4,14 @@ export class Enemy extends Entity {
   health;
   speed;
   aliveTime;
+  hasTurtle;
 
   constructor(ctx, road, sprite) {
     super(ctx, -10, -10, sprite);
     this.road = road;
     this.speed = 0.1;
     this.aliveTime = 0;
+    this.hasTurtle = false;
   }
 
   update(dt) {
