0ee672275c8a556a1aa75a941382cd161305cb02
Julien <jcaillaud17170@gmail.com>
2022-01-08 12:22:53 +0100

    Main with roads and play/pause

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { GameController } from "./controller/GameController.mjs";
 import { Level } from "./model/Level.mjs";
+import { Road } from "./model/Road.mjs";
+
 const width = 1920,
   height = 1080;
 
@@ -29,8 +31,42 @@ resizeCanvases();
 
 const levels = [];
 
+levels.push(
+  new Level("background-1.jpg", [
+    new Road(
+      {
+        x1: width,
+        y1: height / 6,
+        x2: (width * 5) / 6,
+        y2: (height * 2) / 6,
+        x3: (width * 2) / 3,
+        y3: (height * 2) / 6,
+        x4: width / 2,
+        y4: height / 2,
+      },
+      {
+        x1: width / 2,
+        y1: height / 2,
+        x2: width / 3,
+        y2: (height * 4) / 6,
+        x3: width / 6,
+        y3: (height * 4) / 6,
+        x4: 40,
+        y4: height / 2,
+      }
+    ),
+  ])
+);
+
 const gameController = new GameController();
 
 gameController.setLevel(levels[0]);
 
 gameController.start();
+
+window.addEventListener("keydown", (e) => {
+  if (e.key == " ") {
+    if (gameController.paused) gameController.play();
+    else gameController.pause();
+  }
+});
