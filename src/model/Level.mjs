dfd02cb67a52a131b050f3cb42e717b2471a479b
Julien <jcaillaud17170@gmail.com>
2022-01-08 12:22:42 +0100

    Level V0

---
+++ src/model/Level.mjs
@@ -0,0 +1,54 @@
+import { Enemy } from "./Enemy.mjs";
+
+export class Level {
+  background;
+  roads;
+
+  enemiesCtx;
+
+  aliveTime;
+
+  constructor(background, roads) {
+    this.background = background;
+    this.roads = roads;
+
+    this.enemiesCtx = document.getElementById("enemies").getContext("2d");
+
+    this.aliveTime = 0;
+
+    this.nbEnemies = 0;
+  }
+  start() {
+    document.getElementById("background").src = "./assets/" + this.background;
+    const ctx = document.getElementById("roads").getContext("2d");
+
+    ctx.strokeStyle = "#9cec5b";
+    ctx.lineWidth = 40;
+    ctx.lineCap = "round";
+
+    //context.lineTo(canvas.width, canvas.height);
+    for (const road of this.roads) {
+      for (const curve of road.curves) {
+        ctx.beginPath();
+        ctx.moveTo(curve.x1, curve.y1);
+        ctx.bezierCurveTo(
+          curve.x2,
+          curve.y2,
+          curve.x3,
+          curve.y3,
+          curve.x4,
+          curve.y4
+        );
+        ctx.stroke();
+      }
+    }
+  }
+  update(dt) {
+    this.aliveTime += dt;
+    if (this.nbEnemies < 1) {
+      this.nbEnemies++;
+      return new Enemy(this.enemiesCtx, this.roads[0], "");
+    }
+    return;
+  }
+}
